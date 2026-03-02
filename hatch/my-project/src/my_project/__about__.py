6403d9f8829e20ab0e9b514b27aad53502becff5
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-03-02 19:24:47 +0500

    hatch (env) setup

---
+++ hatch/my-project/src/my_project/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2026-present waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ hatch/my-project/src/my_project/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2026-present waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
+#
+# SPDX-License-Identifier: MIT
